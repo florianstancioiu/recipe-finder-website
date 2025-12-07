99ce4347d410ca19d847ab94c87414467bb285d2
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-12-07 14:07:05 +0200

    Added cn function for TailwindCSS
    Updated the Button component

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,10 +1,19 @@
+import { cn } from "tailwind-cn";
+
 export type ButtonProps = {
   children: React.ReactNode;
-  className: string;
+  className?: string;
 };
 
 const Button = ({ children, className }: ButtonProps) => {
-  return <button>{children}</button>;
+  const baseClasses =
+    "px-8 py-4 bg-neutral-900 text-neutral-0 rounded-xl cursor-pointer font-bold";
+  const mergedClasses = cn(
+    baseClasses,
+    className !== undefined ? className : ""
+  );
+
+  return <button className={mergedClasses}>{children}</button>;
 };
 
 export default Button;
