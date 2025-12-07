6f17446da3dae9b21c368bee8fa4014c12b46e37
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-12-07 20:13:36 +0200

    Worked on the Healthy "border"

--- src/components/HealthyMeals/HealthyMeals.tsx
+++ src/components/HealthyMeals/HealthyMeals.tsx
@@ -4,7 +4,11 @@ const HealthyMeals = () => {
   return (
     <div className="px-4 text-neutral-900 mb-10">
       <h1 className="font-nunito text-5xl font-bold mb-4">
-        Healthy meals, zero fuss
+        <span className="relative">
+          <span className="absolute block w-full bg-orange-500 h-6 rounded-sm left-0 bottom-2.5 -z-1 opacity-[0.4]"></span>
+          Healthy
+        </span>
+        <span> meals, zero fuss</span>
       </h1>
       <p className="mb-8">
         Discover eight quick, whole-food recipes that you can cook tonight—no
