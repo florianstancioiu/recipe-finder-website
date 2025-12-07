b12269739fa7960b510e2a48eb9513c93350994f
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-12-07 14:08:09 +0200

    Worked on the mobile HealthyMeals component

--- src/components/HealthyMeals/HealthyMeals.tsx
+++ src/components/HealthyMeals/HealthyMeals.tsx
@@ -1,13 +1,16 @@
+import Button from "../UI/Button/Button";
+
 const HealthyMeals = () => {
   return (
-    <div className="px-4 text-neutral-900">
+    <div className="px-4 text-neutral-900 mb-10">
       <h1 className="font-nunito text-5xl font-bold mb-4">
         Healthy meals, zero fuss
       </h1>
-      <p>
+      <p className="mb-8">
         Discover eight quick, whole-food recipes that you can cook tonight—no
         processed junk, no guesswork.
       </p>
+      <Button>Start exploring</Button>
     </div>
   );
 };
