cef2435e853e96ecd2270e8c7134d4d320c0a213
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-12-08 08:36:35 +0200

    Worked on the Menu mobile component

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -1,14 +1,34 @@
+import { useState, useEffect } from "react";
 import LogoSvg from "../../../images/logo.svg?react";
 import HamburgerMenuSvg from "../../../images/icon-hamburger-menu.svg?react";
+import Menu from "../Menu/Menu";
+import { NavLink, useLocation } from "react-router";
 
 const Header = () => {
+  const location = useLocation();
+  const [showMenu, setShowMenu] = useState(false);
+  const onClickHandler = () => {
+    setShowMenu((val) => !val);
+  };
+
+  // hide the menu when the location changes
+  useEffect(() => {
+    setShowMenu(false);
+  }, [location]);
+
   return (
-    <header className="border-red border-b border-neutral-300 p-4">
-      <div className="flex justify-between items-center">
-        <LogoSvg className="cursor-pointer" />
-        <div className="bg-neutral-300 h-10 w-10 grid place-content-center rounded cursor-pointer">
+    <header className="border-red border-b border-neutral-300 p-4 ">
+      <div className="flex justify-between items-center relative">
+        <NavLink to="/">
+          <LogoSvg className="cursor-pointer" />
+        </NavLink>
+        <div
+          onClick={onClickHandler}
+          className="bg-neutral-300 h-10 w-10 grid place-content-center rounded cursor-pointer"
+        >
           <HamburgerMenuSvg />
         </div>
+        {showMenu && <Menu />}
       </div>
     </header>
   );
---
+++ src/components/UI/Menu/Menu.tsx
@@ -0,0 +1,31 @@
+import Button from "../Button/Button";
+import { NavLink } from "react-router";
+
+const Menu = () => {
+  return (
+    <div className="absolute left-0 top-14 z-10 w-full">
+      <div className="bg-neutral-0 border border-neutral-300 rounded-xl p-2">
+        <ul className="list-none">
+          <li className="px-2 py-3">
+            <NavLink to="/" className={`text-strong-950`}>
+              Home
+            </NavLink>
+          </li>
+          <li className="px-2 py-3">
+            <NavLink to="/about" className={`text-strong-950`}>
+              About
+            </NavLink>
+          </li>
+          <li className="px-2 py-3">
+            <NavLink to="/recipes" className={`text-strong-950`}>
+              Recipes
+            </NavLink>
+          </li>
+        </ul>
+        <Button className="w-full">Browse recipes</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
