4c2a9939a5e3f4a6f27c9eddbe9ffee34f417672
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-12-12 11:24:41 +0200

    Worked on the Breadcrumbs mobile component

--- src/components/UI/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/UI/Breadcrumbs/Breadcrumbs.tsx
@@ -1,5 +1,19 @@
-const Breadcrumbs = () => {
-  return <div></div>;
+import { Link } from "react-router";
+
+export type BreadcrumbsProps = {
+  title: string;
+};
+
+const Breadcrumbs = ({ title }: BreadcrumbsProps) => {
+  return (
+    <div className="text-neutral-900 font-semibold px-4 mb-4">
+      <Link to="/" className="opacity-60">
+        Recipes
+      </Link>
+      <span className="opacity-60"> / </span>
+      <span>{title}</span>
+    </div>
+  );
 };
 
 export default Breadcrumbs;
