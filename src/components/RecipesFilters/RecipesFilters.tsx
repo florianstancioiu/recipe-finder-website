4d01dd33b0c6899499d3ff087824d2e2b5f29ee2
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-12-18 13:50:50 +0200

    Worked on the RecipesFilter components

--- src/components/RecipesFilters/RecipesFilters.tsx
+++ src/components/RecipesFilters/RecipesFilters.tsx
@@ -1,30 +1,86 @@
+import { useState } from "react";
 import Dropdown from "../UI/Dropdown/Dropdown";
 import ChevronDownSvg from "../../images/icon-chevron-down.svg?react";
 import SearchInput from "../UI/SearchInput/SearchInput";
-
-const prepOptions = [
-  { id: 1, value: "0 minutes" },
-  { id: 2, value: "5 minutes" },
-  { id: 3, value: "10 minutes" },
-];
-
-const cookOptions = [
-  { id: 1, value: "0 minutes" },
-  { id: 2, value: "5 minutes" },
-  { id: 3, value: "10 minutes" },
-  { id: 4, value: "15 minutes" },
-  { id: 5, value: "20 minutes" },
-];
+import { type DropdownOption } from "../UI/Dropdown/Dropdown";
 
 const RecipesFilters = () => {
+  const [prepOptions, setPrepOptions] = useState([
+    { id: 1, value: "0 minutes", isActive: false },
+    { id: 2, value: "5 minutes", isActive: false },
+    { id: 3, value: "10 minutes", isActive: false },
+  ]);
+
+  const [cookOptions, setCookOptions] = useState([
+    { id: 1, value: "0 minutes", isActive: false },
+    { id: 2, value: "5 minutes", isActive: false },
+    { id: 3, value: "10 minutes", isActive: false },
+    { id: 4, value: "15 minutes", isActive: false },
+    { id: 5, value: "20 minutes", isActive: false },
+  ]);
+
+  const onPrepClick = (option: DropdownOption) => {
+    setPrepOptions(
+      prepOptions
+        .map((prepOption) => ({ ...prepOption, isActive: false }))
+        .map((prepOption) => {
+          if (prepOption.id === option.id) {
+            return {
+              ...prepOption,
+              isActive: true,
+            };
+          }
+
+          return prepOption;
+        })
+    );
+  };
+  const onPrepClear = () => {
+    setPrepOptions(
+      prepOptions.map((prepOption) => ({ ...prepOption, isActive: false }))
+    );
+  };
+
+  const onCookClick = (option: DropdownOption) => {
+    setCookOptions(
+      cookOptions
+        .map((cookOption) => ({ ...cookOption, isActive: false }))
+        .map((cookOption) => {
+          if (cookOption.id === option.id) {
+            return {
+              ...cookOption,
+              isActive: true,
+            };
+          }
+
+          return cookOption;
+        })
+    );
+  };
+  const onCookClear = () => {
+    setCookOptions(
+      cookOptions.map((cookOption) => ({ ...cookOption, isActive: false }))
+    );
+  };
+
   return (
     <div className="px-4 md:grid md:gap-x-3 md:grid-cols-2 md:px-8 xl:max-w-dsktp-md xl:mx-auto xl:gap-x-[500px] xl:px-0">
       <div className="md:grid-cols-2 md:grid md:gap-x-3">
-        <Dropdown options={prepOptions} className="md:mb-0">
+        <Dropdown
+          options={prepOptions}
+          onOptionClick={onPrepClick}
+          onClear={onPrepClear}
+          className="md:mb-0"
+        >
           <span>Max Prep Time</span>
           <ChevronDownSvg className="inline-block ml-2" />
         </Dropdown>
-        <Dropdown options={cookOptions} className="md:mb-0">
+        <Dropdown
+          options={cookOptions}
+          onOptionClick={onCookClick}
+          onClear={onCookClear}
+          className="md:mb-0"
+        >
           <span>Max Cook Time</span>
           <ChevronDownSvg className="inline-block ml-2" />
         </Dropdown>
--- src/components/UI/Dropdown/Dropdown.tsx
+++ src/components/UI/Dropdown/Dropdown.tsx
@@ -1,32 +1,85 @@
+import { useState } from "react";
 import { cn } from "tailwind-cn";
 
 export type DropdownOption = {
   id: number;
   value: string;
+  isActive: boolean;
 };
 
 export type DropdownProps = {
   children: React.ReactNode;
   options: DropdownOption[];
   className?: string;
+  onOptionClick: (option: DropdownOption) => void;
+  onClear: () => void;
 };
 
-const Dropdown = ({ children, options, className }: DropdownProps) => {
-  const baseClasses = "mb-3";
+const Dropdown = ({
+  children,
+  options,
+  className,
+  onOptionClick,
+  onClear,
+}: DropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const baseClasses = "mb-3 relative";
   const mergedClasses = cn(
     baseClasses,
     className !== undefined ? className : ""
   );
 
+  const onClickTitleHandler = () => {
+    setIsOpen((val) => !val);
+  };
+
+  const optionsClasses = isOpen ? `absolute left-0 top-[58px]` : `hidden`;
+
+  const onOptionClickHandler = (option: DropdownOption) => {
+    setIsOpen(false);
+    onOptionClick(option);
+  };
+
+  const onClearHandler = () => {
+    setIsOpen(false);
+    onClear();
+  };
+
   return (
     <div className={mergedClasses}>
-      <div className="w-full text-center bg-neutral-0 rounded-xl border border-neutral-300 py-2.5 font-normal text-neutral-900 cursor-pointer select-none text-lg">
+      <div
+        onClick={onClickTitleHandler}
+        className="w-full text-center bg-neutral-0 rounded-xl border border-neutral-300 py-2.5 font-normal text-neutral-900 cursor-pointer select-none text-lg"
+      >
         {children}
       </div>
-      <ul className="list-none hidden">
+      <ul
+        className={`list-none bg-neutral-0 w-56 p-2 rounded-lg border border-neutral-400 ${optionsClasses}`}
+      >
         {options.map((option) => (
-          <li key={option.id}>{option.value}</li>
+          <li
+            onClick={() => onOptionClickHandler(option)}
+            key={option.id}
+            className="p-2 select-none cursor-pointer"
+          >
+            {!option.isActive && (
+              <span className="inline-block mr-2 w-4 h-4 border rounded-full border-neutral-400"></span>
+            )}
+            {option.isActive && (
+              <span className="mr-2 w-4 h-4 border rounded-full border-strong-950 inline-grid place-items-center">
+                <span className="w-2 h-2 rounded-full bg-strong-950"></span>
+              </span>
+            )}
+            <span className="text-strong-950">{option.value}</span>
+          </li>
         ))}
+        <li
+          onClick={onClearHandler}
+          className="p-2 pb-0 select-none cursor-pointer text-strong-950"
+        >
+          Clear
+        </li>
       </ul>
     </div>
   );
