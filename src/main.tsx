d9b4f8949f8f140e98ad24fc5de32b7939601828
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-03-01 21:50:01 +0200

    Replace BrowserRouter with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import Routes from "./routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/recipe-finder-website/">
+    <HashRouter>
       <Routes />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
